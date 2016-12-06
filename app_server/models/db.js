c5e4d6d628c6500debd209344b095b6c31252a67
loko <binario200@gmail.com>
2016-12-06 00:03:40 -0600

    adding basic functionallity: list places, reviews

---
+++ app_server/models/db.js
@@ -0,0 +1,48 @@
+var mongoose = require ( 'mongoose' );
+
+require('./locations');
+
+
+var dbURI = 'mongodb://localhost/Flexerz';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connnection error ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+})
+
+
+// reusable function to close Mongoose connection
+gracefullShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+// for nodemon restarts
+process.once('SIGUSR2', function () {
+  gracefullShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+// For app termination
+process.on('SIGINT', function () {
+  gracefullShutdown('app termination', function () {
+    process.exit(0);
+  });
+});
+
+
+//for heroku app termination
+process.on('SIGTERM', function () {
+  gracefullShutdown('Heroku app shutdow', function () {
+    process.exit(0);
+  });
+});
---
+++ app_server/models/locations.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+
+// using mongodb subdocuments
+// subdocuments schemas needs to be before the documents schemas
+var reviewSchema = new mongoose.Schema({
+  author: String,
+  rating: { type: Number, required: true, min: 0, max: 5},
+  reviewText: String,
+  createdOn: { type: Date, "default": Date.now }
+});
+
+var openingTimeSchema = new mongoose.Schema({
+  days: { type: String, required: true },
+  opening: String,
+  closing: String,
+  closed: { type: Boolean, required: true }
+});
+
+var locationSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  address: String,
+  rating: { type: Number, "default": 0, min: 0, max: 5 },
+  facilities: [String],
+  coords: { type: [Number], index: '2dsphere' }, // It allows MongoDB to calculate geometries based on a spherical object.
+
+  openingTimes: [openingTimeSchema],
+  reviews: [reviewSchema]
+});
+
+// To meet the GeoJSON specification, a coordinate pair must be entered into the array in the correct order: longitude then latitude.
+
+// building the model from the schema
+mongoose.model('Location', 'locationSchema', 'Locations');
